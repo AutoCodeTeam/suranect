c5008ea44364fe4c1b79a28250acd26c4c2f947f
Vito Ramadhan <98963921+tejuslemonnn@users.noreply.github.com>
2023-10-22 01:09:05 +0700

    completed ui wisata screen

--- lib/features/wisata/presentation/pages/wisata_screen.dart
+++ lib/features/wisata/presentation/pages/wisata_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:suranect/app/routes/route_utils.dart';
 import 'package:suranect/core/theme/app_colors.dart';
+import 'package:suranect/core/theme/app_shadow.dart';
 import 'package:suranect/core/widgets/base_app_bar.dart';
 import 'package:suranect/core/widgets/base_body_page.dart';
 import 'package:suranect/features/wisata/presentation/widgets/card_wisata_header.dart';
@@ -13,26 +14,96 @@ class WisataScreen extends StatefulWidget {
 }
 
 class _WisataScreenState extends State<WisataScreen> {
+  ScrollController scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseAppBar(title: PAGES.wisata.screenTitle),
-      body: const BaseBodyPage(
+      body: BaseBodyPage(
+        onLoadNextPage: () {},
+        scrollController: scrollController,
+        onRefresh: () async {},
         children: [
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CardWisataHeader(),
-                ],
+          SliverPersistentHeader(
+            delegate: CardWisataHeader(),
+          ),
+          SliverList.separated(
+            itemBuilder: (context, index) => const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: CardWisata(
+                image: 'assets/images/taman_apsari_img.png',
+                title: "Kenpark Surabaya",
+                address:
+                    "Jl. Pantai Ria Kenjeran, Sukolilo Baru, Kec. Bulak, Surabaya, Jawa Timur 60122",
               ),
             ),
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 10,
+            ),
+            itemCount: 10,
           )
         ],
+      ),
+    );
+  }
+}
+
+class CardWisata extends StatelessWidget {
+  final String image;
+  final String title;
+  final String address;
+
+  const CardWisata(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.address});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.125,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        boxShadow: [
+          AppShadow.sShadow,
+        ],
+        color: AppColors.white,
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        child: Row(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: MediaQuery.of(context).size.height * 0.125,
+              child: Image.asset(
+                image,
+                fit: BoxFit.fill,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      address,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/features/wisata/presentation/widgets/card_wisata_header.dart
+++ lib/features/wisata/presentation/widgets/card_wisata_header.dart
@@ -1,71 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:suranect/core/theme/app_colors.dart';
 
-class CardWisataHeader extends StatelessWidget {
-  const CardWisataHeader({super.key});
+class CardWisataHeader extends SliverPersistentHeaderDelegate {
+  @override
+  double get maxExtent => 180;
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.centerLeft,
-      children: [
-        Container(
-          height: MediaQuery.of(context).size.height * 0.2,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                "assets/images/taman_apsari_img.png",
+  double get minExtent => 180;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      oldDelegate.maxExtent != maxExtent || oldDelegate.minExtent != minExtent;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.2,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/taman_apsari_img.png",
+                ),
+                fit: BoxFit.fill,
               ),
-              fit: BoxFit.fill,
-            ),
-            borderRadius: BorderRadius.all(
-              Radius.circular(15),
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
             ),
           ),
-        ),
-        Positioned.fill(
-          child: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black.withOpacity(0.5),
-                    AppColors.black.withOpacity(0.1),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                borderRadius: const BorderRadius.all(Radius.circular(15))),
-          ),
-        ),
-        Positioned(
-          left: 20,
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-            width: MediaQuery.of(context).size.width * 0.5,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Wisata kota Surabaya!",
-                  style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                    color: AppColors.white,
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black.withOpacity(0.5),
+                      AppColors.black.withOpacity(0.1),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
                   ),
-                ),
-                const SizedBox(height: 5),
-                Expanded(
-                  child: Text(
-                    "Yuk cari informasi mengenai wisata di  kota Surabaya.",
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      color: AppColors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(15))),
+            ),
+          ),
+          Positioned(
+            left: 20,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Wisata kota Surabaya!",
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                          color: AppColors.white,
+                        ),
+                  ),
+                  const SizedBox(height: 5),
+                  Expanded(
+                    child: Text(
+                      "Yuk cari informasi mengenai wisata di  kota Surabaya.",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: AppColors.white,
+                          ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
