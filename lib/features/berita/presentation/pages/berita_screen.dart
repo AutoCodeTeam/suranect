7197e9f8a040489753acf01a27bcb3f0b4f94507
Vito Ramadhan <98963921+tejuslemonnn@users.noreply.github.com>
2023-10-22 02:45:46 +0700

    fix berita  ui

--- lib/features/berita/presentation/pages/berita_screen.dart
+++ lib/features/berita/presentation/pages/berita_screen.dart
@@ -5,6 +5,7 @@ import 'package:suranect/core/theme/app_shadow.dart';
 import 'package:suranect/core/utils/scale_size.dart';
 import 'package:suranect/core/widgets/base_app_bar.dart';
 import 'package:suranect/core/widgets/base_body_page.dart';
+import 'package:suranect/features/berita/presentation/widgets/berita_header.dart';
 import 'package:suranect/features/berita/presentation/widgets/card_news.dart';
 import 'package:suranect/features/berita/presentation/widgets/card_latest_news.dart';
 
@@ -105,88 +106,50 @@ class _BeritaScreenState extends State<BeritaScreen> {
         scrollController: scrollController,
         onLoadNextPage: () {},
         children: [
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: AppColors.white,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        boxShadow: [
-                          AppShadow.sShadow,
-                        ]),
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: chips,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Divider(
-                    thickness: 2,
-                    color: AppColors.neutral_30,
-                  ),
-                  const SizedBox(height: 10),
-                  CardNews(
-                    image: "assets/images/news_example.png",
-                    title:
-                        "Wali Kota Surabaya perbaiki rumah terbakar dengan target 10 hari pada hari itu",
-                    subtitle:
-                        "Wali Kota Surabaya Eri Cahyadi targetkan perbaikan rumah warga yang terbakar di Kelurahan Putat Jaya, Kecamatan Sawahan. Wali Kota Surabaya Eri Cahyadi targetkan perbaikan rumah warga yang terbakar di Kelurahan Putat Jaya, Kecamatan Sawahan",
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 5,
-                      shrinkWrap: true,
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(width: 10),
-                      itemBuilder: (context, index) => CardNews(
-                        image: "assets/images/news_example.png",
-                        title: "Surabaya, Tanjungan membuka festival tahunan",
-                        height: MediaQuery.of(context).size.height * 0.2,
-                        width: MediaQuery.of(context).size.height * 0.2,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    "Berita Terbaru",
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.35,
-                    child: ListView.separated(
-                      itemBuilder: (context, index) => const CardLatestNews(
-                        image: "assets/images/news_example.png",
-                        title:
-                            "#SobatAir, PDAM Surya embada menerima kunjungan studi dari mahasisa S2 Fakultas Kesehatan Masyarakat dan",
-                        subtitle:
-                            "Jembatan tol Juanda-Waru baru saja dimulai pagi ini, pada tanggal 23 July 2023 dan akan terdapat pengarahan lalu lintas...",
-                      ),
-                      separatorBuilder: (context, index) => const SizedBox(
-                        width: 10,
-                      ),
-                      itemCount: 5,
-                      scrollDirection: Axis.horizontal,
-                    ),
-                  )
-                ],
+          SliverPersistentHeader(
+            delegate: BeritaHeader(
+              chips: chips,
+              cardHeader: CardNews(
+                image: "assets/images/news_example.png",
+                title:
+                    "Wali Kota Surabaya perbaiki rumah terbakar dengan target 10 hari pada hari itu",
+                subtitle:
+                    "Wali Kota Surabaya Eri Cahyadi targetkan perbaikan rumah warga yang terbakar di Kelurahan Putat Jaya, Kecamatan Sawahan. Wali Kota Surabaya Eri Cahyadi targetkan perbaikan rumah warga yang terbakar di Kelurahan Putat Jaya, Kecamatan Sawahan",
+                height: MediaQuery.of(context).size.height * 0.3,
+                width: MediaQuery.of(context).size.width,
+              ),
+              listNews: ListView.separated(
+                itemBuilder: (context, index) => const CardLatestNews(
+                  image: "assets/images/news_example.png",
+                  title:
+                      "#SobatAir, PDAM Surya embada menerima kunjungan studi dari mahasisa S2 Fakultas Kesehatan Masyarakat dan",
+                  subtitle:
+                      "Jembatan tol Juanda-Waru baru saja dimulai pagi ini, pada tanggal 23 July 2023 dan akan terdapat pengarahan lalu lintas...",
+                ),
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 10,
+                ),
+                itemCount: 5,
+                scrollDirection: Axis.vertical,
               ),
             ),
-          )
+          ),
+          SliverList.separated(
+            itemBuilder: (context, index) => const Padding(
+              padding: EdgeInsets.only(bottom: 15.0,left: 15.0, right: 15.0),
+              child: CardLatestNews(
+                image: "assets/images/news_example.png",
+                title:
+                "#SobatAir, PDAM Surya embada menerima kunjungan studi dari mahasisa S2 Fakultas Kesehatan Masyarakat dan",
+                subtitle:
+                "Jembatan tol Juanda-Waru baru saja dimulai pagi ini, pada tanggal 23 July 2023 dan akan terdapat pengarahan lalu lintas...",
+              ),
+            ),
+            separatorBuilder: (context, index) => const SizedBox(
+              width: 10,
+            ),
+            itemCount: 5,
+          ),
         ],
       ),
     );
---
+++ lib/features/berita/presentation/widgets/berita_header.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:suranect/core/theme/app_colors.dart';
+import 'package:suranect/core/theme/app_shadow.dart';
+import 'package:suranect/features/berita/presentation/widgets/card_news.dart';
+
+class BeritaHeader extends SliverPersistentHeaderDelegate {
+  @override
+  double get maxExtent => 570;
+
+  @override
+  double get minExtent => 570;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      oldDelegate.maxExtent != maxExtent || oldDelegate.minExtent != minExtent;
+
+  final List<Widget> chips;
+  final Widget cardHeader;
+  final Widget listNews;
+
+  const BeritaHeader({
+    required this.chips,
+    required this.cardHeader,
+    required this.listNews,
+  });
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 15.0,left: 15.0, right: 15.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.05,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius:
+              const BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                AppShadow.sShadow,
+              ]),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: chips,
+          ),
+        ),
+        const SizedBox(height: 10),
+        cardHeader,
+        const Divider(
+          thickness: 2,
+          color: AppColors.neutral_30,
+        ),
+        const SizedBox(height: 10),
+        const SizedBox(height: 10),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.2,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            shrinkWrap: true,
+            separatorBuilder: (context, index) =>
+            const SizedBox(width: 10),
+            itemBuilder: (context, index) => CardNews(
+              image: "assets/images/news_example.png",
+              title: "Surabaya, Tanjungan membuka festival tahunan",
+              height: MediaQuery.of(context).size.height * 0.2,
+              width: MediaQuery.of(context).size.height * 0.2,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          "Berita Terbaru",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ],
+    ),
+  );
+  }
+
+}
